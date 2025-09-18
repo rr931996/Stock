8ae1fd780a89f54045cb363b3c9f0582778ebd47
Aditya <aditya.goel.gis@gmail.com>
2025-09-18 20:22:49 +0530

    Stock Current Price

--- routes/yahoo.js
+++ routes/yahoo.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const fetchStockData = require("../lib/fetchStock");
+const { fetchStockData, getCurrentPrice } = require("../lib/fetchStock");
 
 const router = express.Router();
+
+// GET current price from Yahoo
+router.get("/price/:symbol", async (req, res) => {
+  try {
+    const { symbol } = req.params;
+    const data = await getCurrentPrice(symbol.toUpperCase());
+    res.json({ source: "Yahoo Finance", data });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 // GET live stock from Yahoo and store in DB
 router.get("/:symbol", async (req, res) => {
